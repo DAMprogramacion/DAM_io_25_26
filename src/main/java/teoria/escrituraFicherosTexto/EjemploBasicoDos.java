27a2fcc61cd0aa5488851b2c73503686ba323d31
ManuelMolino <mmolmil158@g.educaand.es>
2026-04-20 09:38:35 +0200

    Inicio del tema

---
+++ src/main/java/teoria/escrituraFicherosTexto/EjemploBasicoDos.java
@@ -0,0 +1,14 @@
+package teoria.escrituraFicherosTexto;
+
+public class EjemploBasicoDos {
+    public static void main(String[] args) {
+        //scanner leer el fichero ficheroUno.txt
+        //escribimos en un fichero llamado ficheroUnoAnalizado.txt
+        //la salida sería:
+            //leido 49 bytes
+            //leido dos líneas
+            //linea 1: Texto a escribir en fichero y una ñ
+            //línea 2: nueva línea
+            //fecha: fecha actual en formato dd/mm/aaaa
+    }
+}
---
+++ src/main/java/teoria/escrituraFicherosTexto/EjemploBasicoUno.java
@@ -0,0 +1,22 @@
+package teoria.escrituraFicherosTexto;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class EjemploBasicoUno {
+    public static void main(String[] args) {
+        Path outPath = Paths.get("ficheros/ficheroUno.txt");
+        String texto = "Texto a escribir en fichero y una ñ\n";
+        try {
+            Files.writeString(outPath, texto);
+            Files.writeString(outPath, texto); //sobreescribe el fichero
+            Files.writeString(outPath, "nueva línea", StandardOpenOption.APPEND);
+            System.out.printf("Escritos %d bytes en %s%n", Files.size(outPath), outPath);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+}
