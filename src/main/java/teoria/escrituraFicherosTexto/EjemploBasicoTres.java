570ec04b88be6741604c15838e7cff84f2815ad6
ManuelMolino <mmolmil158@g.educaand.es>
2026-04-21 13:22:35 +0200

    Lectura fichero, subida fichero numbers.txt

---
+++ src/main/java/teoria/escrituraFicherosTexto/EjemploBasicoTres.java
@@ -0,0 +1,32 @@
+package teoria.escrituraFicherosTexto;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class EjemploBasicoTres {
+    public static void main(String[] args) {
+        Path inPath = Paths.get("ficheros/quijote.txt");
+        try {
+            String contenido  = Files.readString(inPath);
+            System.out.printf("Tamaño del fichero leído: %d bytes%n", Files.size(inPath));
+            System.out.printf("Caracteres leídos: %d%n", contenido.length());
+            //quiero saber cuantas veces aparece la palabra Quijote en el texto
+            String[] tokens = contenido.split("[\s\n]");
+            //System.out.println(Arrays.toString(tokens)); comprobaciĺn
+            int contadorQuijotes = 0;
+            for (String palabra : tokens) {
+                if (palabra.toLowerCase().contains("quijote")) {
+                    System.out.println(palabra);
+                    contadorQuijotes++;
+                }
+            }
+            System.out.printf("Nº veces palabra quijote %d%n", contadorQuijotes);
+        } catch (IOException e) {
+            System.err.println(e);;
+        }
+        System.out.println("Fin de programa");
+    }
+}
